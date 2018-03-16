9883efd032f417602d71de345b0c602bf9695064
Huyen-Trang <huyentranghathi@gmail.com>
2018-03-17 02:29:14 +0700

    first

---
+++ app/src/main/java/tranghth/calculation/NumberOperator.java
@@ -0,0 +1,32 @@
+package tranghth.calculation;
+
+/**
+ * Created by TrangHT on 28/012/2017.
+ */
+
+public class NumberOperator {
+
+    private int number;
+    private String operator;
+
+    public NumberOperator(int number, String operator) {
+        this.number = number;
+        this.operator = operator;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+}
